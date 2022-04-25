34ca374d9aa0a4e41a181addcf6cc964906ebdbd
TJ Moore <tj.moore@crunchydata.com>
2022-04-25 15:23:40 -0400

    Convert container security context test to kuttl
    This creates a Kuttl test to verify a PostgresCluster's containers'
    security context settings which replaces the current EnvTest
    implementation.
    
    Issue: [sc-14262]

--- internal/controller/postgrescluster/cluster_test.go
+++ internal/controller/postgrescluster/cluster_test.go
@@ -20,10 +20,7 @@ package postgrescluster
 
 import (
 	"context"
-	"os"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
@@ -37,7 +34,6 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -616,93 +612,6 @@ func TestCustomAnnotations(t *testing.T) {
 			}
 		}
 	})
-}
-
-func TestContainerSecurityContext(t *testing.T) {
-	if !strings.EqualFold(os.Getenv("USE_EXISTING_CLUSTER"), "true") {
-		t.Skip("Test requires pods to be created")
-	}
-
-	t.Parallel()
-
-	env, cc, config := setupTestEnv(t, ControllerName)
-	t.Cleanup(func() { teardownTestEnv(t, env) })
-
-	reconciler := &Reconciler{}
-	ctx, cancel := setupManager(t, config, func(mgr manager.Manager) {
-		reconciler = &Reconciler{
-			Client:   cc,
-			Owner:    client.FieldOwner(t.Name()),
-			Recorder: mgr.GetEventRecorderFor(ControllerName),
-			Tracer:   otel.Tracer(t.Name()),
-		}
-		podExec, err := newPodExecutor(config)
-		assert.NilError(t, err)
-		reconciler.PodExec = podExec
-	})
-	t.Cleanup(func() { teardownManager(cancel, t) })
-
-	ns := &corev1.Namespace{}
-	ns.GenerateName = "postgres-operator-test-"
-	ns.Labels = labels.Set{"postgres-operator-test": ""}
-	assert.NilError(t, cc.Create(ctx, ns))
-	t.Cleanup(func() { assert.Check(t, cc.Delete(ctx, ns)) })
-
-	cluster := testCluster()
-	cluster.Namespace = ns.Name
-
-	assert.NilError(t, errors.WithStack(reconciler.Client.Create(ctx, cluster)))
-	t.Cleanup(func() {
-		// Remove finalizers, if any, so the namespace can terminate.
-		assert.Check(t, client.IgnoreNotFound(
-			reconciler.Client.Patch(ctx, cluster, client.RawPatch(
-				client.Merge.Type(), []byte(`{"metadata":{"finalizers":[]}}`)))))
-	})
-
-	pods := &corev1.PodList{}
-	assert.NilError(t, wait.Poll(time.Second, Scale(2*time.Minute), func() (bool, error) {
-		// Reconcile the cluster
-		result, err := reconciler.Reconcile(ctx, reconcile.Request{
-			NamespacedName: client.ObjectKeyFromObject(cluster),
-		})
-		if err != nil {
-			return false, err
-		}
-		if result.Requeue {
-			return false, nil
-		}
-
-		err = reconciler.Client.List(ctx, pods,
-			client.InNamespace(ns.Name),
-			client.MatchingLabels{
-				naming.LabelCluster: cluster.Name,
-			})
-		if err != nil {
-			return false, err
-		}
-
-		// Can expect 4 pods from a cluster
-		// instance, repo-host, pgbouncer, backup(s)
-		if len(pods.Items) < 4 {
-			return false, nil
-		}
-		return true, nil
-	}))
-
-	// Once we have a pod list with pods of each type, check that the
-	// pods containers have the expected Security Context options
-	for _, pod := range pods.Items {
-		for _, container := range pod.Spec.Containers {
-			assert.Equal(t, *container.SecurityContext.Privileged, false)
-			assert.Equal(t, *container.SecurityContext.ReadOnlyRootFilesystem, true)
-			assert.Equal(t, *container.SecurityContext.AllowPrivilegeEscalation, false)
-		}
-		for _, initContainer := range pod.Spec.InitContainers {
-			assert.Equal(t, *initContainer.SecurityContext.Privileged, false)
-			assert.Equal(t, *initContainer.SecurityContext.ReadOnlyRootFilesystem, true)
-			assert.Equal(t, *initContainer.SecurityContext.AllowPrivilegeEscalation, false)
-		}
-	}
 }
 
 func TestGenerateClusterPrimaryService(t *testing.T) {
